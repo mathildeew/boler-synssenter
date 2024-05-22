845bc430208a1788da559a4fe5373955413aec1f
Mathilde Wiik <mathildewiik@hotmail.com>
2024-05-22 18:44:35 +0200

    media quieres for eyechech section

--- app/components/Home/EyeCheck.jsx
+++ app/components/Home/EyeCheck.jsx
@@ -2,14 +2,16 @@ import Link from "next/link";
 
 export default function EyeCheck() {
   return (
-    <section className="bg-lightBeige px-4 py-16 flex flex-col gap-8 items-center justify-center md:flex-row md:justify-between md:px-10 md:py-28">
-      <img className=" h-72 object-cover rounded-3xl md:w-1/3" src="harpreet-singh-ioYYWWX2fjk_downsized.jpg" />
-      <div className="flex flex-col gap-6 items-center">
-        <h1 className="text-darkPurple">Ta vare på synet ditt</h1>
-        <p>Å passe på synet er avgjørende for å opprettholde god livskvalitet og forebygge alvorlige øyesykdommer. Regelmessige synsundersøkelser kan avdekke tidlige tegn på problemer som kan behandles før de utvikler seg, og sikre at du alltid har riktig brille- eller kontaktlinsekorreksjon for best mulig syn.</p>
-        <Link href="/synsundersokelse" className="baseButton text-lightBeige ">
-          Bestill synsundersøkelse
-        </Link>
+    <section className="w-full bg-lightBeige px-4 py-16 flex justify-center md:px-10 md:py-28 ">
+      <div className="max-w-[1750px] grid md:grid-cols-2 md:items-center md:gap-12">
+        <img className="object-cover rounded-3xl " src="harpreet-singh-ioYYWWX2fjk_downsized.jpg" />
+        <div className="flex flex-col gap-6 items-center">
+          <h1 className="text-darkPurple">Ta vare på synet ditt</h1>
+          <p>Å passe på synet er avgjørende for å opprettholde god livskvalitet og forebygge alvorlige øyesykdommer. Regelmessige synsundersøkelser kan avdekke tidlige tegn på problemer som kan behandles før de utvikler seg, og sikre at du alltid har riktig brille- eller kontaktlinsekorreksjon for best mulig syn.</p>
+          <Link href="/synsundersokelse" className="baseButton text-lightBeige ">
+            Bestill synsundersøkelse
+          </Link>
+        </div>
       </div>
     </section>
   );
