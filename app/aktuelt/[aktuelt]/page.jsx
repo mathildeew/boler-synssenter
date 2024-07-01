858618b6a50de5abae971a00db87ffe765257745
Mathilde Wiik <mathildewiik@hotmail.com>
2024-07-01 23:03:08 +0200

    fetching one article done

--- app/aktuelt/[aktuelt]/page.jsx
+++ app/aktuelt/[aktuelt]/page.jsx
@@ -1,3 +1,4 @@
+import { PortableText } from "next-sanity";
 import { getDocument } from "../../../sanity/sanity-utils";
 
 export default async function Article({ params }) {
@@ -6,8 +7,11 @@ export default async function Article({ params }) {
   const article = await getDocument(slug);
   console.log(article);
   return (
-    <section>
+    <section className="max-w-[1460px] px-4 py-16 md:px-10 md:py-28">
       <h1>{article.name}</h1>
+      <PortableText value={article.intro} />
+      <img src={article.image} alt={article.name} />
+      <PortableText value={article.content} />
     </section>
   );
 }
