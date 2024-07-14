517ad93b92f7303d90c5cdf67c464731eae76b53
Mathilde Wiik <mathildewiik@hotmail.com>
2024-07-14 15:06:55 +0200

    eyecheck section done

--- app/components/Home/EyeCheck.jsx
+++ app/components/Home/EyeCheck.jsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 export default function EyeCheck() {
   return (
     <section className="bg-lightBeige w-full flex justify-center px-4 py-16 md:px-10 md:py-28 ">
-      <div className="max-w-[1460px] grid md:grid-cols-2 md:items-center md:gap-12">
-        <img className="object-cover rounded-3xl " src="harpreet-singh-ioYYWWX2fjk_downsized.jpg" />
-        <div className="flex flex-col gap-6 items-center">
-          <h1 className="text-darkPurple">Ta vare på synet ditt</h1>
+      <div className="max-w-[1460px] grid gap-12 md:grid-cols-2 md:items-center">
+        <img className="object-cover rounded-3xl" src="harpreet-singh-ioYYWWX2fjk_downsized.jpg" />
+        <div className="flex flex-col gap-8 items-center">
+          <h1 className="text-5xl text-center font-serif text-darkPurple">Ta vare på synet ditt</h1>
           <p>Å passe på synet er avgjørende for å opprettholde god livskvalitet og forebygge alvorlige øyesykdommer. Regelmessige synsundersøkelser kan avdekke tidlige tegn på problemer som kan behandles før de utvikler seg, og sikre at du alltid har riktig brille- eller kontaktlinsekorreksjon for best mulig syn.</p>
           <Link href="bestill-time" className="baseButton text-lightBeige ">
-            Bestill synsundersøkelse
+            Bestill time
           </Link>
         </div>
       </div>
--- app/components/Home/Hero.jsx
+++ app/components/Home/Hero.jsx
@@ -1,7 +1,8 @@
 export default function Hero() {
   return (
-    <section className="CMS w-full h-[1000px] flex items-center justify-center">
-      <p className="text-3xl text-skyBlue font-bold tracking-widest">Eget innhold fra CMS</p>
+    <section className="CMS w-full h-[500px] flex items-center justify-center">
+      <h1 className="text-5xl text-center font-serif">Velkommen til Bøler Synssenter!</h1>
+      {/* Putt inn bilde av undersøkelse og teskt "velkommen til bøler synssenter??" */}
     </section>
   );
 }
