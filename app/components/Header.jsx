221ffe5f3eabffc0f0960ffcbb7f1ed7c78acd12
Mathilde Wiik <mathildewiik@hotmail.com>
2024-07-15 22:35:06 +0200

    nav styling

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,6 +1,8 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faL, faLongArrowRight } from "@fortawesome/free-solid-svg-icons";
 import BolerLogo from "./BolerLogo";
 
 export default function Header() {
@@ -9,7 +11,12 @@ export default function Header() {
   return (
     <>
       <header className="relative lg:flex lg:justify-between z-20">
-        <div className="bg-white w-full h-20 max-w-[1460px] px-4 mx-auto flex items-center justify-between lg:z-auto lg:w-full lg:h-32 lg:px-10">
+        <div className="bg-white w-full h-16 max-w-[1460px] mx-auto flex items-center justify-between px-4 lg:z-auto lg:w-full lg:px-10">
+          <div className="hidden lg:flex w-full justify-end items-center gap-4">
+            <FontAwesomeIcon icon={faLongArrowRight} />
+            <a>Kunde? Bestill kontaktlinser</a>
+          </div>
+
           <Link href="/" className="flex flex-col gap-1 items-center">
             <BolerLogo />
             <span className="text-darkPurple font-medium uppercase tracking-wider hidden lg:inline">Bøler Synssenter</span>
@@ -39,15 +46,15 @@ export default function Header() {
               <Link href="aktuelt" data-animate-in="true" data-animation-order="4">
                 Aktuelt
               </Link>
-              <a className="bg-lightPurple rounded-full px-8 py-2 w-fit" data-animate-in="true" data-animation-order="5">
+              {/* <a className="bg-lightPurple rounded-full px-8 py-2 w-fit" data-animate-in="true" data-animation-order="5">
                 Bestill kontaktlinser
-              </a>
+              </a> */}
             </div>
           </nav>
         </div>
       </header>
 
-      <nav className={`bg-white w-full absolute translate-all ease-in-out duration-700 z-10 lg:inline lg:translate-y-0  lg:relative lg:w-auto ${openMenu ? "top-20 h-screen" : "-top-[400px] h-20 lg:h-auto"}`}>
+      <nav className={`bg-white w-full absolute translate-all ease-in-out duration-700 z-10 lg:inline lg:translate-y-0 lg:relative lg:w-auto ${openMenu ? "top-20 h-screen" : "-top-[400px] h-20 lg:h-auto"}`}>
         <div className="flex flex-col items-end gap-12 px-4 py-12 lg:hidden">
           <Link href="/synsundersokelse" className="px-6">
             Synsundersøkelse
--- app/components/Home/Hero.jsx
+++ app/components/Home/Hero.jsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <section className="CMS w-full h-[500px] flex items-center justify-center">
-      <h1 className="text-5xl text-center font-serif">Velkommen til Bøler Synssenter!</h1>
+      <h1 className="text-base  md:text-5xl text-center font-serif">Velkommen til Bøler Synssenter!</h1>
       {/* Putt inn bilde av undersøkelse og teskt "velkommen til bøler synssenter??" */}
     </section>
   );
