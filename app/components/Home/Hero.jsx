c615248932f6e762966bb4f98745eb8a4f4f654a
Mathilde Wiik <mathildewiik@hotmail.com>
2024-07-16 14:14:21 +0200

    foreløpig hero done

--- app/components/Home/Hero.jsx
+++ app/components/Home/Hero.jsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 export default function Hero() {
   return (
-    <section className="CMS w-full h-[500px] flex items-center justify-center">
-      <h1 className="text-base  md:text-5xl text-center font-serif">Velkommen til Bøler Synssenter!</h1>
-      {/* Putt inn bilde av undersøkelse og teskt "velkommen til bøler synssenter??" */}
+    <section className="CMS bg-lightBeige w-full h-[500px] flex flex-col justify-end lg:items-end">
+      <div className="bg-lightBlue bg-opacity-70 w-full py-8 flex flex-col justify-center items-center gap-8 px-4 lg:w-[500px] lg:h-full">
+        <h1 className="text-base  md:text-5xl text-center font-serif">Syn og øyehelse - hva er viktig?</h1>
+        <p>Eget innhold fra CMS her? Eller tom for linser?</p>
+        <Link href="/" className="baseButton">
+          Les mer
+        </Link>
+      </div>
     </section>
   );
 }
