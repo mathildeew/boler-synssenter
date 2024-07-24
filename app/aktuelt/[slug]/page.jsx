0a932cb849d76d0449b0e76f2a77e27200ab214e
Mathilde Wiik <mathildewiik@hotmail.com>
2024-07-24 18:38:41 +0200

    adds loader

--- app/aktuelt/[slug]/page.jsx
+++ app/aktuelt/[slug]/page.jsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import apiQueries from "../../../sanity/apiQueries";
 import useAPI from "../../hooks/useAPI";
 import { useCallback, useState, useEffect } from "react";
+import Loader from "../../components/Loader/Loader";
 
 export default function Article({ params }) {
   const [article, setArticle] = useState([]);
@@ -25,22 +26,26 @@ export default function Article({ params }) {
   console.log(article);
 
   return (
-    <article className="text-darkPurple flex flex-col items-center gap-12 px-4 py-16 md:px-10 md:py-28">
-      <Link href="/aktuelt" className="hover:underline">
-        ← Aktuelt
-      </Link>
+    <>
+      {isLoading && <Loader />}
+
       {isSuccess && (
-        <>
-          <section className="max-w-[1000px] flex flex-col gap-6" data-animate-in="true" data-animation-order="1">
-            <h1>{article.name}</h1>
-            <span className="text-xl text-center md:text-3xl">{article.intro}</span>
-            <img src={urlFor(article.image).url()} alt={article.image.alt} className="max-h-[550px] object-cover object-center rounded-xl" />
-          </section>
-          <section className="max-w-[700px]">
-            <PortableText value={article.content} components={PortableTextComponents} />
-          </section>
-        </>
+        <article className="text-darkPurple flex flex-col items-center gap-12 px-4 py-16 md:px-10 md:py-28">
+          <Link href="/aktuelt" className="hover:underline">
+            ← Aktuelt
+          </Link>
+          <>
+            <section className="max-w-[1000px] flex flex-col gap-6" data-animate-in="true" data-animation-order="1">
+              <h1>{article.name}</h1>
+              <span className="text-xl text-center md:text-3xl">{article.intro}</span>
+              <img src={urlFor(article.image).url()} alt={article.image.alt} className="max-h-[550px] object-cover object-center rounded-xl" />
+            </section>
+            <section className="max-w-[700px]">
+              <PortableText value={article.content} components={PortableTextComponents} />
+            </section>
+          </>
+        </article>
       )}
-    </article>
+    </>
   );
 }
