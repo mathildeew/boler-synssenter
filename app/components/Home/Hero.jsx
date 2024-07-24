7497d51d33ebf98672869c97d6b01a009fab4f2c
Mathilde Wiik <mathildewiik@hotmail.com>
2024-07-24 16:23:13 +0200

    refactored and adds loader

--- app/components/Home/Hero.jsx
+++ app/components/Home/Hero.jsx
@@ -4,38 +4,21 @@ import { useCallback, useEffect, useState } from "react";
 import apiQueries from "../../../sanity/apiQueries";
 import useAPI from "../../hooks/useAPI";
 
-// Revalidate the page every 60 seconds
-export const revalidate = 60;
-
-export default function Hero() {
-  const [hero, setHero] = useState([]);
-  const { fetchApi, isLoading, isSuccess, isError, errorMsg } = useAPI();
-
-  const getData = useCallback(async () => {
-    const result = await fetchApi(apiQueries().hero);
-    setHero(result);
-  });
-
-  useEffect(() => {
-    getData();
-  }, []);
+export default function Hero({ data }) {
+  const { title, text, imageUrl, imageAlt, link } = data;
 
   return (
-    <>
-      {isSuccess && (
-        <section className="w-full lg:h-[500px]" data-animate-in="true" data-animation-order="1">
-          <div className="h-full grid gap-12 lg:grid-cols-2 md:items-center">
-            <img className="object-cover w-full h-full" src={hero.imageUrl} alt={hero.imageAlt} />
-            <div className="text-darkPurple flex flex-col gap-10 items-center px-4 pb-16 md:px-10 lg:py-20">
-              <h1>{hero.title}</h1>
-              <span className="text-2xl text-center">{hero.text}</span>
-              <Link href="bestill-time" className="baseButton text-lightBeige">
-                Bestill time
-              </Link>
-            </div>
-          </div>
-        </section>
-      )}
-    </>
+    <section className="w-full lg:h-[500px]">
+      <div className="h-full grid gap-12 lg:grid-cols-2 md:items-center">
+        <img className="object-cover w-full h-full" src={imageUrl} alt={imageAlt} />
+        <div className="text-darkPurple flex flex-col gap-10 items-center px-4 pb-16 md:px-10 lg:py-20">
+          <h1>{title}</h1>
+          <span className="text-2xl text-center">{text}</span>
+          <Link href={link.url} className="baseButton text-lightBeige">
+            {link.urlText}
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,35 @@
-// "use client";
+"use client";
 import Hero from "./components/Home/Hero";
 import Services from "./components/Home/Services";
 import News from "./components/Home/News";
+import { useCallback, useEffect, useState } from "react";
+import apiQueries from "../sanity/apiQueries";
+import useAPI from "./hooks/useAPI";
+import Loader from "./components/Loader/Loader";
 
 export default function Home() {
+  const [heroData, setHeroData] = useState([]);
+  const { fetchApi, isLoading, isSuccess, isError, errorMsg } = useAPI();
+
+  const getData = useCallback(async () => {
+    const result = await fetchApi(apiQueries().hero);
+    setHeroData(result);
+  });
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <>
-      <Hero />
-      <Services />
-      <News />
+      {isLoading && <Loader />}
+
+      {isSuccess && (
+        <div data-animate-in="true" data-animation-order="1">
+          <Hero data={heroData} />
+          <Services />
+          <News />
+        </div>
+      )}
     </>
   );
 }
