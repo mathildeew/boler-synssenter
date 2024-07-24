6dae33ee1ff4589f45d2b0893de61508471c0b41
Mathilde Wiik <mathildewiik@hotmail.com>
2024-07-24 14:39:34 +0200

    displays hero content from sanity

--- app/components/Home/Hero.jsx
+++ app/components/Home/Hero.jsx
@@ -1,18 +1,41 @@
+"use client";
 import Link from "next/link";
+import { useCallback, useEffect, useState } from "react";
+import apiQueries from "../../../sanity/apiQueries";
+import useAPI from "../../hooks/useAPI";
+
+// Revalidate the page every 60 seconds
+export const revalidate = 60;
 
 export default function Hero() {
+  const [hero, setHero] = useState([]);
+  const { fetchApi, isLoading, isSuccess, isError, errorMsg } = useAPI();
+
+  const getData = useCallback(async () => {
+    const result = await fetchApi(apiQueries().hero);
+    setHero(result);
+  });
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <section className="w-full flex justify-center px-4 py-16 md:px-10 md:py-28">
-      <div className="max-w-[1460px] grid gap-12 md:grid-cols-2 md:items-center">
-        <img className="object-cover rounded-3xl" src="/alexander-grey-NeRKgBUUDjM-downsized.jpg" />
-        <div className="text-darkPurple flex flex-col gap-10 items-center">
-          <h1 className="">Ta vare på synet ditt</h1>
-          <span className="text-2xl">Å passe på synet er avgjørende for å opprettholde god livskvalitet og forebygge alvorlige øyesykdommer</span>
-          <Link href="bestill-time" className="baseButton text-lightBeige">
-            Bestill time
-          </Link>
-        </div>
-      </div>
-    </section>
+    <>
+      {isSuccess && (
+        <section className="w-full lg:h-[500px]" data-animate-in="true" data-animation-order="1">
+          <div className="h-full grid gap-12 lg:grid-cols-2 md:items-center">
+            <img className="object-cover w-full h-full" src={hero.imageUrl} alt={hero.imageAlt} />
+            <div className="text-darkPurple flex flex-col gap-10 items-center px-4 pb-16 md:px-10 lg:py-20">
+              <h1>{hero.title}</h1>
+              <span className="text-2xl text-center">{hero.text}</span>
+              <Link href="bestill-time" className="baseButton text-lightBeige">
+                Bestill time
+              </Link>
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   );
 }
