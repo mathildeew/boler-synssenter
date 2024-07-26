9eae5f50f34ba2a999e903e06a2430dd17193558
Mathilde Wiik <mathildewiik@hotmail.com>
2024-07-26 12:55:34 +0200

    adds new styles and animation

--- app/components/Loader/Loader.jsx
+++ app/components/Loader/Loader.jsx
@@ -1,10 +1,10 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
+import {SpinnerIcon} from '@sanity/icons'
 
 export default function Loader() {
   return (
-    <section className="h-screen text-darkPurple flex flex-col gap-6  items-center justify-center">
-      <FontAwesomeIcon icon={faCircleNotch} size="3x" className="animate-spin" data-animate-in="true" data-animation-order="1" />
+    <section className="h-screen text-darkPurple flex flex-col gap-6 items-center justify-center">
+      
+      <div className="animate-pulse text-xl font-sans ">Laster inn...</div>
     </section>
   );
 }
