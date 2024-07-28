cf9f4a70ac97220351b755ea4f14da869d57adb1
Mathilde Wiik <mathildewiik@hotmail.com>
2024-07-28 21:30:55 +0200

    adds not found

---
+++ app/not-found.jsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCat } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+
+export default function PageNotFound() {
+  return (
+    <section className="text-darkPurple h-[700px] border-2 flex flex-col items-center justify-center gap-10 px-4 py-16 md:px-10 md:py-20">
+      <h1>Side ikke funnet</h1>
+      <FontAwesomeIcon icon={faCat} size="3x" />
+      <Link href="/" className="text-xl font-sans tracking-wider hover:underline hover:underline-offset-2">
+        Gå til forsiden
+      </Link>
+    </section>
+  );
+}
