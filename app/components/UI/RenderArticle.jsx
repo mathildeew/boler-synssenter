d3eb526c962be70e5a7f1de089dd2303e41114a8
Mathilde Wiik <mathildewiik@hotmail.com>
2024-07-30 11:32:02 +0200

    fix images

--- app/components/UI/RenderArticle.jsx
+++ app/components/UI/RenderArticle.jsx
@@ -8,8 +8,8 @@ export default function RenderArticle({ title, article }) {
       <section className="w-full  max-w-[1000px] flex flex-col gap-10 mb-6">
         <h1>{title}</h1>
         <span className="text-xl text-center md:text-3xl">{article.intro}</span>
-        <div className="w-full object-cover rounded-xl overflow-hidden">
-          <Image src={article.imageUrl} alt={article.imageAlt} width={100} height={100} layout="responsive" />
+        <div className="w-full h-[600px] object-cover rounded-xl overflow-hidden">
+          <Image src={article.imageUrl} alt={article.imageAlt} width={1000} height={1000} style={{ width: "full", height: "full", objectFit: "contain", objectPosition: "center center" }} />
         </div>
       </section>
       <section className="max-w-[700px] mx-auto">
