e5c4aa9bb5d5d5f0d6b3bf777cea310627296e46
Mathilde Wiik <mathildewiik@hotmail.com>
2024-07-30 11:32:49 +0200

    remove global layout from studio

---
+++ app/ClientLayout.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+// Layout utils
+import { usePathname } from "next/navigation";
+import ScrollToTop from "./utils/ScrollToTop";
+
+// Context & components
+import { StoreInfoProvider } from "./context/StoreInfoProvider";
+import Header from "./components/UI/Header";
+import Footer from "./components/UI/Footer";
+
+export default function ClientLayout({ children }) {
+  const pathname = usePathname();
+  const isStudio = pathname.startsWith("/admin");
+
+  return (
+    <>
+      {isStudio ? (
+        <main>{children}</main>
+      ) : (
+        <StoreInfoProvider>
+          <ScrollToTop />
+          <Header />
+          <main className="h-full min-h-screen flex flex-col items-center pb-20">{children}</main>
+          <Footer />
+        </StoreInfoProvider>
+      )}
+    </>
+  );
+}
--- app/layout.jsx
+++ app/layout.jsx
@@ -5,12 +5,7 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
 // Layout utils
-import ScrollToTop from "./utils/ScrollToTop";
-
-// Context & components
-import { StoreInfoProvider } from "./context/StoreInfoProvider";
-import Header from "./components/UI/Header";
-import Footer from "./components/UI/Footer";
+import ClientLayout from "./ClientLayout";
 
 // Static metadata
 export const metadata = {
@@ -23,12 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="no">
       <body>
-        <StoreInfoProvider>
-          <ScrollToTop />
-          <Header />
-          <main className="flex flex-col items-center pb-20">{children}</main>
-          <Footer />
-        </StoreInfoProvider>
+        <ClientLayout>{children}</ClientLayout>
       </body>
     </html>
   );
