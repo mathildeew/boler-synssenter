e67f50dc607bd9f40c726d3c21481e55239860ac
Mathilde Wiik <mathildewiik@hotmail.com>
2024-08-31 18:46:46 +0200

    adds brands to sanity

--- app/components/Brands/RenderBrands.jsx
+++ app/components/Brands/RenderBrands.jsx
@@ -24,7 +24,7 @@ export default function OurBrands() {
     <section className="w-full grid gap-10 md:grid-cols-2 lg:grid-cols-3">
       {brands.map((brand) => (
         <div key={brand._id} className="flex flex-col gap-2">
-          <div className="h-36 relative">
+          <div className="h-48 relative md:h-36">
             <Image src={urlFor(brand.carousel[0].image.asset._ref).url()} alt={brand.carousel[0].altText} objectFit="cover" fill sizes="auto" className="w-full h-full rounded-xl" />
           </div>
           <a href={brand.url} className="flex gap-2 items-center hover:underline">
