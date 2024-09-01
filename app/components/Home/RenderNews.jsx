bb257cb3c2bc6a1c3c1c5cdf7e10589254d2c5c8
Mathilde Wiik <mathildewiik@hotmail.com>
2024-09-01 22:12:13 +0200

    articles overflows

--- app/components/Home/RenderNews.jsx
+++ app/components/Home/RenderNews.jsx
@@ -7,33 +7,35 @@ export default function RenderNews(data) {
   const [articleId, setArticleId] = useState(null);
 
   return (
-    <div className="max-w-[1460px] grid gap-6 grid-cols-news overflow-auto scroll-snap-mandatory gridExpand:grid-cols-2 md:gap-6 lg:grid-cols-4">
-      {articles.map((article) => {
-        return (
-          <Link
-            key={article._id}
-            href={`/aktuelt/${article.slug}`}
-            onMouseEnter={() => {
-              setArticleId(article._id);
-            }}
-            onMouseLeave={() => {
-              setArticleId(null);
-            }}
-          >
-            <div className="text-darkPurple flex flex-col gap-3 snap-center items-center">
-              <div className="w-full h-72 rounded-3xl overflow-hidden">
-                <div className={`w-full h-72 transition-all ease-in-out duration-300 overflow-hidden ${articleId === article._id ? "scale-[110%]" : "scale-1"}`}>
-                  <Image src={article.image} alt={article.name} width={1000} height={1000} style={{ width: "100%", height: "100%", objectFit: "cover" }} />
+    <div className="block mx-auto px-0 w-full">
+      <div className="max-w-[1460px] w-full grid grid-cols-news gap-6 overflow-x-hidden snap-[x mandatory] gridExpand:grid-cols-2 lg:grid-cols-4 border-2 border-red-500">
+        {articles.map((article) => {
+          return (
+            <Link
+              key={article._id}
+              href={`/aktuelt/${article.slug}`}
+              onMouseEnter={() => {
+                setArticleId(article._id);
+              }}
+              onMouseLeave={() => {
+                setArticleId(null);
+              }}
+            >
+              <div className="text-darkPurple w-full h-full border-2 border-green-500 flex flex-col gap-3 snap-center items-center">
+                <div className="w-full rounded-3xl overflow-hidden">
+                  <div className={`w-auto h-72 transition-all ease-in-out duration-300 overflow-hidden ${articleId === article._id ? "scale-[110%]" : "scale-1"}`}>
+                    <Image src={article.image} alt={article.name} width={1000} height={1000} style={{ width: "100%", height: "100%", objectFit: "cover" }} />
+                  </div>
                 </div>
+                {/* <div className="text-left block border-2 border-blue-500">
+                  <h3>{article.name}</h3>
+                  <p>{article.intro}</p>
+                </div> */}
               </div>
-              <div className="text-left w-full">
-                <h3 className="text-left">{article.name}</h3>
-                <p>{article.intro}</p>
-              </div>
-            </div>
-          </Link>
-        );
-      })}
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
