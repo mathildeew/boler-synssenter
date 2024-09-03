1454290497acda3af3cb9438ac481e75102eed55
Mathilde Wiik <mathildewiik@hotmail.com>
2024-09-03 20:10:51 +0200

    hides scrollbar

--- app/components/Home/RenderNews.jsx
+++ app/components/Home/RenderNews.jsx
@@ -6,7 +6,7 @@ export default function RenderNews(data) {
   const [articleId, setArticleId] = useState(null);
 
   return (
-    <div className="max-w-[1460px] w-full grid grid-cols-news gap-6 overflow-x-auto snap-[x mandatory] gridExpand:grid-cols-2 lg:grid-cols-4">
+    <div className="max-w-[1460px] w-full grid grid-cols-news gap-6 overflow-x-auto snap-[x mandatory] gridExpand:grid-cols-2 lg:grid-cols-4 cardGrid">
       {articles.map((article) => {
         return (
           <Link
@@ -20,13 +20,13 @@ export default function RenderNews(data) {
             }}
             className="snap-center"
           >
-            <div className="text-darkPurple w-full h-full grid gap-3 items-center">
+            <div className="text-darkPurple w-full h-fit grid grid-rows-1 gap-3 overflow-hidden">
               <div className="w-full rounded-3xl overflow-hidden">
                 <div className={`w-auto h-72 transition-all ease-in-out duration-300 overflow-hidden ${articleId === article._id ? "scale-[110%]" : "scale-1"}`}>
                   <Image src={article.image} alt={article.name} width={1000} height={1000} style={{ width: "100%", height: "100%", objectFit: "cover" }} />
                 </div>
               </div>
-              <div className="text-left line-clamp-6">
+              <div className="text-left h-fit line-clamp-6">
                 <h3>{article.name}</h3>
                 <p>{article.intro}</p>
               </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,7 @@ module.exports = {
       screens: {
         gridExpand: "600px",
       },
+      
     },
   },
   plugins: [],
