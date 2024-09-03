bfcb90fc2a5acae80499b8a46950948de55bb320
Mathilde Wiik <mathildewiik@hotmail.com>
2024-09-03 19:07:34 +0200

    adds snap

--- app/components/Home/RenderNews.jsx
+++ app/components/Home/RenderNews.jsx
@@ -6,35 +6,34 @@ export default function RenderNews(data) {
   const [articleId, setArticleId] = useState(null);
 
   return (
-    <div className="block mx-auto px-0 w-full">
-      <div className="max-w-[1460px] w-full grid grid-cols-news gap-6 overflow-x-hidden snap-[x mandatory] gridExpand:grid-cols-2 lg:grid-cols-4">
-        {articles.map((article) => {
-          return (
-            <Link
-              key={article._id}
-              href={`/aktuelt/${article.slug}`}
-              onMouseEnter={() => {
-                setArticleId(article._id);
-              }}
-              onMouseLeave={() => {
-                setArticleId(null);
-              }}
-            >
-              <div className="text-darkPurple w-full h-full grid gap-3 snap-center items-center">
-                <div className="w-full rounded-3xl overflow-hidden">
-                  <div className={`w-auto h-72 transition-all ease-in-out duration-300 overflow-hidden ${articleId === article._id ? "scale-[110%]" : "scale-1"}`}>
-                    <Image src={article.image} alt={article.name} width={1000} height={1000} style={{ width: "100%", height: "100%", objectFit: "cover" }} />
-                  </div>
-                </div>
-                <div className="text-left line-clamp-6">
-                  <h3>{article.name}</h3>
-                  <p>{article.intro}</p>
+    <div className="max-w-[1460px] w-full grid grid-cols-news gap-6 overflow-x-hidden snap-[x mandatory] gridExpand:grid-cols-2 lg:grid-cols-4">
+      {articles.map((article) => {
+        return (
+          <Link
+            key={article._id}
+            href={`/aktuelt/${article.slug}`}
+            onMouseEnter={() => {
+              setArticleId(article._id);
+            }}
+            onMouseLeave={() => {
+              setArticleId(null);
+            }}
+            className="snap-center"
+          >
+            <div className="text-darkPurple w-full h-full grid gap-3 items-center">
+              <div className="w-full rounded-3xl overflow-hidden">
+                <div className={`w-auto h-72 transition-all ease-in-out duration-300 overflow-hidden ${articleId === article._id ? "scale-[110%]" : "scale-1"}`}>
+                  <Image src={article.image} alt={article.name} width={1000} height={1000} style={{ width: "100%", height: "100%", objectFit: "cover" }} />
                 </div>
               </div>
-            </Link>
-          );
-        })}
-      </div>
+              <div className="text-left line-clamp-6">
+                <h3>{article.name}</h3>
+                <p>{article.intro}</p>
+              </div>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
