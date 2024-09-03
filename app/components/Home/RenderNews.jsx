98fdd1557a5595f44011298b28bb3f293d0ea064
Mathilde Wiik <mathildewiik@hotmail.com>
2024-09-03 20:34:52 +0200

    snap center

--- app/components/Home/RenderNews.jsx
+++ app/components/Home/RenderNews.jsx
@@ -18,9 +18,8 @@ export default function RenderNews(data) {
             onMouseLeave={() => {
               setArticleId(null);
             }}
-            className="snap-center"
           >
-            <div className="text-darkPurple w-full h-fit grid grid-rows-1 gap-3 overflow-hidden">
+            <div className="text-darkPurple w-full h-fit grid grid-rows-1 gap-3 snap-center overflow-hidden">
               <div className="w-full rounded-3xl overflow-hidden">
                 <div className={`w-auto h-72 transition-all ease-in-out duration-300 overflow-hidden ${articleId === article._id ? "scale-[110%]" : "scale-1"}`}>
                   <Image src={article.image} alt={article.name} width={1000} height={1000} style={{ width: "100%", height: "100%", objectFit: "cover" }} />
