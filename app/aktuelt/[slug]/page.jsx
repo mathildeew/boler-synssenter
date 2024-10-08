2464947c4b69f58f576348ba84b4897b73456ea0
Mathilde Wiik <mathildewiik@hotmail.com>
2024-10-08 10:29:35 +0200

    remove staticparams

--- app/aktuelt/[slug]/page.jsx
+++ app/aktuelt/[slug]/page.jsx
@@ -1,4 +1,3 @@
-import { client } from "../../../sanity/sanity-utils";
 import { getMetadata } from "../../hooks/getMetadata";
 import ArticleComponent from "../../components/News/[slug]/ArticleComponent";
 
@@ -8,18 +7,6 @@ export const revalidate = 60;
 export async function generateMetadata({ params }) {
   const slug = params.slug;
   return await getMetadata("article", slug);
-}
-
-export async function generateStaticParams() {
-  const slugs = await client.fetch(`
-    *[_type == "article"]{
-      "slug": slug.current
-    }
-  `);
-
-  return slugs.map((article) => ({
-    slug: article.slug,
-  }));
 }
 
 export default function Article({ params }) {
