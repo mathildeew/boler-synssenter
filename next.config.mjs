e7c35d74a1eaf6cae465c6e8595b9acfdbfa3d14
Mathilde Wiik <mathildewiik@hotmail.com>
2024-10-08 12:16:24 +0200

    adds metadataBase

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"], // Legg til domenet du bruker
   },
+  experimental: new URL("www.https://boler-synssenter.vercel.app/"),
 };
 
 export default nextConfig;
