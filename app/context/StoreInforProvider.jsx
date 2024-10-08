00ab13b1fe4d5a65099452601cb1ef819657f1a9
Mathilde Wiik <mathildewiik@hotmail.com>
2024-10-08 10:33:02 +0200

    adds jsdocs

--- app/context/StoreInforProvider.jsx
+++ app/context/StoreInforProvider.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { createContext, useContext, useEffect, useState, useCallback, useMemo } from "react";
+import React, { createContext, useContext, useEffect, useState, useMemo } from "react";
 import useAPI from "../hooks/useAPI";
 import apiQueries from "../../sanity/apiQueries";
 
--- app/hooks/getMetadata.jsx
+++ app/hooks/getMetadata.jsx
@@ -1,6 +1,15 @@
 import { client } from "../../sanity/sanity-utils";
 import apiQueries from "../../sanity/apiQueries";
 
+/**
+ * Fetches metadata based on a query and a slug.
+ *
+ * @param {string} query - Key to fetch a specific metadata query.
+ * @param {string} slug - Unique identifier used to fetch the metadata.
+ * @returns {Object} - An object containing the title and description.
+ * @returns {string} title - The title of the metadata.
+ * @returns {string} description - The description of the metadata.
+ */
 export async function getMetadata(query, slug) {
   const metadata = await client.fetch(apiQueries(slug)[query]);
 
