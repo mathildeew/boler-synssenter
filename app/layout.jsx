8dc516a3b03d73729c7bccaadfe2cadb6a848a4f
Mathilde Wiik <mathildewiik@hotmail.com>
2024-12-11 13:44:04 +0100

    adds google tag manager to all pages

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
-// Styles
+// Import styles and other modules
 import "./globals.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { GoogleTagManager } from "@next/third-parties/google";
+
 config.autoAddCss = false;
 
 // Layout utils
@@ -17,6 +19,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="no">
+      <GoogleTagManager gtmId="GTM-MJF75MS2" />
+
       <body>
         <ClientLayout>{children}</ClientLayout>
       </body>
--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ const nextConfig = {
   images: {
     domains: ["cdn.sanity.io"], // Legg til domenet du bruker
   },
-  experimental: {
-    metadataBase: new URL("www.https://boler-synssenter.vercel.app/"),
-  },
+  // experimental: {
+  //   metadataBase: new URL("www.https://boler-synssenter.vercel.app/"),
+  // },
 };
 
 export default nextConfig;
