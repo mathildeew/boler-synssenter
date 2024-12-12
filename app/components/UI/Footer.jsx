9a07f104a7fdbf96da681f993a06915d0a20cc36
Mathilde Wiik <mathildewiik@hotmail.com>
2024-12-12 22:07:30 +0100

    clean up

--- app/components/UI/Footer.jsx
+++ app/components/UI/Footer.jsx
@@ -9,7 +9,8 @@ export default function Footer() {
 
   return (
     <footer className="bg-lightBlue w-full grid gap-8 py-10 md:gap-14">
-      <img src="/dotted.svg" alt="" className="w-full" />
+      <img src="/dotted.svg" className="w-full" aria-hidden="true" />
+
       <div className="flex flex-col justify-center px-4 md:px-10 lg:px-16">
         <div className="w-full max-w-[1460px] grid gap-12 md:grid-cols-2">
           <div className="grid">
@@ -21,7 +22,7 @@ export default function Footer() {
               </div>
               <div>
                 <p>Lørdag:</p>
-                <p>{openingHours?.sat}</p>
+                <p>{openingHours.sat}</p>
               </div>
             </div>
           </div>
@@ -30,8 +31,12 @@ export default function Footer() {
             <h3>Kontakt oss</h3>
             <div className="grid gap-4">
               <div className="grid">
-                <a href={`tel:${phone}`}>{phone}</a>
-                <a href={`mailto:${mail}`}>{mail}</a>
+                <a href={`tel:${phone}`} aria-label={`Ring ${phone}`}>
+                  {phone}
+                </a>
+                <a href={`mailto:${mail}`} aria-label={`Send mail til ${mail}`}>
+                  {mail}
+                </a>
               </div>
               <div>
                 <p>{address.street}</p>
