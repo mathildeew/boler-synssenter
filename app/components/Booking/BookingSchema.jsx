2299dc016c41b16ab4821a59689cf00fb349955f
Mathilde Wiik <mathildewiik@hotmail.com>
2024-12-12 22:33:13 +0100

    align text left

--- app/components/Booking/BookingSchema.jsx
+++ app/components/Booking/BookingSchema.jsx
@@ -6,7 +6,7 @@ export default function BookingForm() {
   return (
     <>
       <div className="w-full max-w-[1460px] flex flex-col gap-20 items-center px-4 py-16 md:px-10 md:py-20">
-        <div className="w-full max-w-[1000px] flex flex-col gap-8 items-center md:gap-12">
+        <div className="w-full max-w-[1000px] flex flex-col gap-8 md:gap-12">
           <h1>Bestill time</h1>
           <div className="coptikk-booking-embed max-w-[1400px] h-full px-4 py-10 md:px-10" data-member-no="5099" data-member-id="644" data-domain-name="https://www.coptikk.no"></div>
         </div>
